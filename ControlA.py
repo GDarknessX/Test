2d7920e61a10835ee6ba090d11b4ab8732ba9187
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-10 20:03:16 +0800

    Update ControlA.py

--- ControlA.py
+++ ControlA.py
@@ -23,9 +23,7 @@ if st.button("发送数据到 Arduino"):
 
 received_data = read_data()
 st.write("从 Arduino 接收到的数据:", received_data)
-在此示例中，我们首先导入了 streamlit 和 serial 模块。然后，我们创建了一个简单的 Streamlit 应用，它允许您通过点击按钮将数据发送到 Arduino，并显示从 Arduino 接收到的数据。
 
-请注意，在上述示例中，您需要根据您的实际情况修改串口号（如 'COM3'）和波特率（如 9600）。确保您已经连接了 Arduino 并配置好了串口通信。
 
 
 
