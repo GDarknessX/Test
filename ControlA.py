2c1544da63dc77fe5db76044ac39794ac546d145
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-10 19:52:07 +0800

    Update ControlA.py

--- ControlA.py
+++ ControlA.py
@@ -1,17 +1,28 @@
-import requests
+import serial
+
+# 打开串口连接
+ser = serial.Serial('COM3', 9600)  # 请根据您的串口号和波特率进行设置
+
+# 从串口读取数据
+def read_data():
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            return data
+
+# 向串口写入数据
+def write_data(data):
+    ser.write(data.encode())
+
+# Streamlit 应用
 import streamlit as st
 
-st.title('请先检测脉搏')
+st.title("Arduino 通信示例")
 
-# 创建按钮来控制开关状态
-on_button = st.button('开始')
-off_button = st.button('停止')
+if st.button("发送数据到 Arduino"):
+    write_data("Hello from Streamlit!")
+    st.success("数据已发送")
 
-# 根据按钮状态发送请求到 Flask Web 服务
-if on_button:
-    response = requests.post('http://127.0.0.1:5000/control', data='on')
-    st.write(response.text)
-elif off_button:
-    response = requests.post('http://127.0.0.1:5000/control', data='off')
-    st.write(response.text)
+received_data = read_data()
+st.write("从 Arduino 接收到的数据:", received_data)
 
